707e64818abeab480685e68e7e2275cb3681b708
venkat9507 <venkatraman230@gmail.com>
2021-07-02 15:31:51 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_school_app_july_1/screens/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:SplashScreen(),
     );
   }
 }
